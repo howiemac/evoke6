1da368c9bfd15ef297607dcce1ad55fb882689ff
howie <howiemac@gmail.com>
2017-08-14 11:31:33 +0100

    minor bug-fixes

--- app/code/serve.py
+++ app/code/serve.py
@@ -9,10 +9,6 @@ import os
 import sys
 import imp
 
-# Set the encoding to utf-8
-imp.reload(sys)  # needed to expose setdefaultencoding
-sys.setdefaultencoding('utf-8')
-
 # fix the path
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(1, os.path.abspath('../..'))
