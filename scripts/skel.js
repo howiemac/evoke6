13accf924293fa2dd03c481800846e018739c0de
howie <howiemac@gmail.com>
2017-10-10 12:13:43 +0100

    scripts folder added, with jquery/js template

---
+++ scripts/skel.js
@@ -0,0 +1,41 @@
+;(function($) {
+
+  $(document).ready(function($) {
+    // end of document ready
+    });
+
+   // end of jQuery
+   } )(jQuery);
+
+/*
+a skeleton file / boilerplate template for js files
+
+
+Let's say you were writing a new doessomething.js.
+
+You would
+
+	cp  …/base/scripts/skel.js  ./doessomething.js
+
+Then add your jQuery spaghetti into the relevant gaps.
+
+eg.
+
+	;(function($) {
+	
+	  function doingsomenting() {
+	      alert('I am doing something');
+	      }
+
+	  $(document).ready(function($) {
+
+	    $('button').click(doingsomething);
+
+	    // end of document ready
+	    });
+
+	   // end of jQuery
+	   } )(jQuery);
+
+
+*/
